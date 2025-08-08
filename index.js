028beda289e6b3ea9ac0742e56e3dde6105f0206
TOBIAS SCHNEIDER MARTINS <tobias_martins@estudante.sesisenai.org.br>
2025-08-08 17:34:41 -0300

    Fiz até a 5

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-//login
+//estilo da pagina geral
+document.body.style.textAlign = "center"
+
+//Onde salvo os dados
 function entrar(){
 
     var nome = document.getElementById("nome").value;
+    var cor = document.getElementById("cor").value;
+    var num = document.getElementById("num").value;
+    var texto = document.getElementById("texto").value;
 
     localStorage.setItem("nome", nome);
-
+    localStorage.setItem("cor", cor);
+    localStorage.setItem("num", num);
+    localStorage.setItem("texto", texto);
+    
     window.location.href = "home.html";
-
+    
 }
 
-//home
 
+//home
 const nome = localStorage.getItem("nome");
+const cor = localStorage.getItem("cor");
+const num = localStorage.getItem("num");
+const texto = localStorage.getItem("texto");
+
 document.getElementById("mensagem").textContent = "Bem vindo, " + nome + "!";
+document.getElementById("msg_num").textContent = "Cujo seu número favorito é " + num + "!";
+document.getElementById("msg_txt").textContent = "Você escreveu: " + texto;
+
+document.body.style.backgroundColor = cor
 
 function sair(){
 
